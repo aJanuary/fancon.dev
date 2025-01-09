f330530a8975b7787c09716b5988f3f4e1aecd88
Andrew January <andrewjanuary@gmail.com>
2025-01-09 17:31:55 +0000

    Redesign layout
    Redesign the layout of the page to be more aesthetically pleasing. This
    makes heavier use of gradients and a hero image.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "fancon.dev",
-  description:
-    "A Discord community of people who are involved in software for fan-run conventions.",
+  description: "Discord community for fan-run convention software",
 };
 
 export default function RootLayout({
@@ -18,8 +17,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} m-auto max-w-prose bg-olive p-4 text-white antialiased`}
+        className={`${inter.className} mx-auto flex min-h-screen flex-col bg-gradient-to-br from-midnight-moss-950 via-midnight-moss-900 to-midnight-moss-950 p-0 text-white antialiased`}
       >
+        <nav className="flex h-10 flex-row items-center gap-12 bg-black px-4 py-2 text-sm text-green-50">
+          <h1>
+            <span
+              className="bg-gradient-to-br from-lemon to-cyan bg-clip-text font-bold text-transparent"
+              aria-hidden
+            >
+              {"{}"}
+            </span>{" "}
+            fancon.dev
+          </h1>
+        </nav>
+
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,28 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <h1 className="mb-4 inline-block bg-gradient-to-br from-lemon to-cyan bg-clip-text text-5xl font-bold text-transparent sm:text-6xl">
-        fancon.dev
-      </h1>
-
-      <p className="mb-4">
-        A Discord community of people who are involved in software for fan-run
-        conventions.
-      </p>
-      <p className="mb-4">
-        We have people who are involved in con software at all levels, from
-        requirements gathering and pecurement through to development, support,
-        usability and testing. Whether you have worked on dozens of cons, or are
-        just interested in how to get involved, we would love to have you join
-        us. Interested bystanders are also welcome.
-      </p>
-      <div className="inline-block rounded-full bg-gradient-to-br from-lemon to-cyan p-px">
-        <Link
-          className="active:shadow-button-glow hover:shadow-button-glow transition:ease-in-out inline-block rounded-full bg-olive px-4 py-2 transition-all hover:bg-transparent hover:text-olive active:bg-transparent active:text-olive"
-          href="/discord-invite"
-        >
-          Join the Discord
-        </Link>
+      <div className="min-h-[calc(100vh-2.5rem)] bg-cover bg-[left_30rem_center] bg-no-repeat md:bg-[url('/discord.svg')] xl:bg-[left_50rem_center]">
+        <div className="mx-10 mb-5 mt-10 max-w-md xl:mx-20 xl:mb-10 xl:mt-20 xl:max-w-3xl">
+          <h2 className="3xl:text-8xl mb-4 text-4xl tracking-wide xl:text-6xl">
+            Discord community for fan-run convention software
+          </h2>
+          <p className="mb-2 max-w-xl">
+            Developers, designers, requirements engineers, support, usability
+            experts, testers, and more.
+          </p>
+          <p className="mb-4 max-w-xl">
+            Whether you&apos;ve worked on dozens of cons, or just interested in
+            how to get involved, we&apos;d love to have you join us.
+          </p>
+          <div className="inline-block rounded-full bg-gradient-to-br from-lemon to-cyan p-[2px]">
+            <Link
+              className="transition:ease-in-out active:text-midnight-moss-50 inline-block rounded-full bg-[#05160e] px-4 py-2 transition-all hover:bg-transparent hover:text-midnight-moss-950 hover:shadow-button-glow active:bg-transparent active:shadow-button-glow"
+              href="/discord-invite"
+            >
+              Join the Discord
+            </Link>
+          </div>
+        </div>
       </div>
     </>
   );
